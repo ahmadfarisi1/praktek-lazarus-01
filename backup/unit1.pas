fafb258bb0a38238210728d2dea06ef17cfc2beb
someone <ahmadfarisi12345@gmail.com>
2020-04-26 22:05:31 +0800

    operasi tipe data

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -33,8 +33,35 @@ implementation
 { TForm1 }
 
 procedure TForm1.Button1Click(Sender: TObject);
+var
+   namaDepan: String;
+   namaTengah, namaBelakang: String;
+   Usia: Integer;
+   targetKuliah: Integer;
+   IPK: Double;
+   nilaiAbjad: Char;
+   tampan: Boolean;
 begin
- Showmessage('Hello '+enama.text);
+     namaDepan:='Ahmad';
+     namaTengah:='Far';
+     namaBelakang:='esy';
+     usia:=18;
+     IPK:= 3.896572431;
+     nilaiAbjad:='A';
+     tampan:=False;
+
+     //MessageDlg('Judul Pesan',namaDepan + ' ' +
+     //      namaTengah + namaBelakang,MtInformation,[mBOK],0);
+     //
+     //MessageDlg('Usia',IntTostr(usia),MtInformation,[mBOK],0);
+
+     //MessageDlg('IPK',FloatTostrF(IPK,ffFixed,3,2),MtInformation,[mBOK],0);
+
+     //MessageDlg('Tampan',BoolTostr(tampan),MtInformation,[mBOK],0);
+
+     MessageDlg('Hari ini',FormatDateTime('ddd, dd-mmmm-yyyy'),MtInformation,[mBOK],0);
+
+
 end;
 
 procedure TForm1.Label1Click(Sender: TObject);
